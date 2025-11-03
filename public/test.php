096ec98eea029ac7330bf91a30431eeaf99c19d1
spikasis <spikasis@gmail.com>
2025-11-03 21:00:37 +0200

    Add debug pages for diagnosing PHP execution and CodeIgniter loading

---
+++ public/test.php
@@ -0,0 +1,6 @@
+<?php
+echo "PHP is working! Time: " . date('Y-m-d H:i:s');
+echo "<br>Document Root: " . $_SERVER['DOCUMENT_ROOT'];
+echo "<br>Current Dir: " . __DIR__;
+echo "<br>This file: " . __FILE__;
+?>
