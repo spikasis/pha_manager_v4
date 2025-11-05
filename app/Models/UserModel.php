e05c27bba0df611ca834d6e4cf70023030cc3c9b
spikasis <spikasis@gmail.com>
2025-11-05 23:18:41 +0200

    Add UserModel class to manage user data and dashboard statistics

---
+++ app/Models/UserModel.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+namespace App\Models;
+
+use App\Models\BaseCRUDModel;
+
+class UserModel extends BaseCRUDModel
+{
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    protected $displayField = 'full_name';
+    
+    public function getDashboardStats()
+    {
+        return [
+            'total_users' => 0,
+            'active_users' => 0,
+            'inactive_users' => 0
+        ];
+    }
+}
