b81c8201044c05332dc8b1ebd096119f4d19fb08
spikasis <spikasis@gmail.com>
2025-11-08 07:32:23 +0200

    Update base URL for local development and add session files for testing

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://manager.pikasishearing.gr/';
+$config['base_url'] = 'http://localhost:8080/';
 
 /*
 |--------------------------------------------------------------------------
